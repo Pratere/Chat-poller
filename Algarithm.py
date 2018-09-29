218adb33615c1e188b51d487ddc425b7cc2c4980
Eli Prater <eliprater@Elis-MacBook-Pro.local>
2018-09-29 15:33:56 -0400

    fix sorting and counting words

--- Algarithm.py
+++ Algarithm.py
@@ -1,43 +1,55 @@
-test1 = ["String", "String", "Strong", "Strang", "String"]
+from difflib import SequenceMatcher
+
+test1 = ["String", "String", "Strong", "Strang", "String", "Booty"]
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 def getSimilars(list1):
-    startDict = {}
+    startDict = []
     print(startDict)
     for string in list1:
+        found = False
         stringl = string.lower()
-        req = len(string) - (len(string)//5)
+        stringl = ''.join(i for i in stringl if i.isalnum())
         if len(startDict) == 0:
-            stringlDict = stringl + "Dict"
-            startDict[stringlDict] = {stringl:1}
-        for dict in startDict:
-            print(dict)
-            if stringl in dict:
-                dict[stringl] += 1
-            else:
-                found = False
-                for key in dict:
-                    count = 0
-                    if len(stringl) <= key:
-                        length = len(stringl)
-                        found = True
-                    else:
-                        length = len(key)
-                    for i in range(length):
-                        if stringl[i] == key[i]:
-                            count+=1
-                    if count >= req:
-                        dict[stringl] = 1
-                        found = True
-                if not found:
-                    stringlDict = stringl + "Dict"
-                    startDict[stringlDict] = {}
+            startDict.append({stringl:1})
+            print(stringl, "put in new dict", startDict[0])
+            found = True
+        else:
+            for dict in startDict:
+                if stringl in dict:
+                    print(stringl, "found in", dict)
+                    dict[stringl] += 1
+                    found = True
+                    break
+                else:
+                    for key in dict:
+                        if similar(stringl, key) >= 0.75:
+                            print(stringl, "similar word found in", dict)
+                            dict[stringl] = 1
+                            found = True
+                            break
+                    if found:
+                        break
+        if not found:
+            startDict.append({stringl:1})
+            print(stringl, "put in new dict")
+    return startDict
+
+def getTopWords(list):
+    topWords = {}
+    for dict in list:
+        top = 0
+        total = 0
+        for key in dict:
+            if dict[key] > top:
+                top = dict[key]
+                topKey = key
+            total += dict[key]
+
 
 def main():
     getSimilars(test1)
-    # dict = {"Hello": 1}
-    # dict["Hello"] += 1
-    # print(dict)
 
 main()
-
-!chatr 
