1198059144c7a846b5ca059e064371f34cdf35ca
Eli Prater <eliprater@Elis-MacBook-Pro.local>
2018-09-29 12:28:13 -0400

    Improve algorithem to strip spaces and use difflib to check similairties between strings

--- Algarithm.py
+++ Algarithm.py
@@ -1,5 +1,8 @@
 from difflib import SequenceMatcher
 test1 = ["String", "String", "Strong", "Strang", "String", "Wrong", "Wrang", "Wong", "Dong", "String", "String", "Strong", "Strang", "String", "Wrong", "Wrang", "Wong", "Dong", "Clong"]
+test2 = [" String", "string", "Strwing", "Strong ", "Sta ir", "Stare", "s Tor"]
+test3 = ["String", "String", "Strong", "Strang", "String", "Wrong", "Wrang", "Wong", "Dong", "String", "String", "Strong", "Strang", "String", "Wrong", "Wrang", "Wong", "Dong", "Clong", " String", "string", "Strwing", "Strong ", "Sta ir", "Stare", "s Tor"]
+
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
@@ -7,45 +10,55 @@ def getSimilars(list1):
     startDict = []
     for string in list1:
         found = False
-        print(string)
-        stringl = string.lower()
+        # print(string)
+        stringl = string.lower().replace(" ", "")
         req = len(string) - (len(string)//5)
         if len(startDict) == 0:
             startDict.append({stringl:1})
-            print(stringl, "Added to new Dict")
+            # print(stringl, "Added to new Dict")
             found = True
         else:
             for dict in startDict:
                 if stringl in dict:
                     dict[stringl] += 1
-                    print(stringl, "Found in Dict:", dict)
+                    # print(stringl, "Found in Dict:", dict)
                     found = True
                     break
                 else:
                     for key in dict:
-                        # count = 0
-                        # if len(stringl) <= len(key):
-                        #     length = len(stringl)
-                        # else:
-                        #     length = len(key)
-                        # for i in range(length):
-                        #     if stringl[i] == key[i]:
-                        #         count+=1
-                        # if count >= req:
                         if similar(stringl, key) >= 0.75:
                             dict[stringl] = 1
                             found = True
-                            print(stringl, "Found to be similar in Dict")
+                            # print(stringl, "Found to be similar in Dict")
                             break
             if not found:
                 startDict.append({stringl:1})
-                print(stringl, "Added to new Dict")
+                # print(stringl, "Added to new Dict")
 
 
-    print(startDict)
+    return startDict
+
+def getTopWords(listOfDicts):
+    topWords = {}
+    for dict in listOfDicts:
+        top = 0
+        total = 0
+        for key in dict:
+            if dict[key] > top:
+                top = dict[key]
+                topKey = key
+            total += dict[key]
+        if total in topWords:
+            topWords[total] += " + " + topKey
+        else:
+            topWords[total] = topKey
+
+    return topWords
 
 def main():
-    getSimilars(test1)
-    print(similar("wong", "dong"))
+    print(getTopWords(getSimilars(test1)))
+    print(getTopWords(getSimilars(test2)))
+    print(getTopWords(getSimilars(test3)))
 
-main()
+if __name__ == '__main__':
+    main()
